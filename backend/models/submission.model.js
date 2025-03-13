6b47252fa7cd1646549fb564dc71d49a76f20306
Imran Khan <khanimran092606@gmail.com>
2025-03-13 19:14:32 -0400

    submission schema

---
+++ backend/models/submission.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const submissionSchema = new mongoose.Schema(
+  {
+    title:{
+        type: String,
+        requred: true,
+    },
+    abstract:{
+        type: String,
+        requred: true,
+    },
+    keywords:{
+        type: [String],
+        requred: true,
+    },
+    authors:{
+        type: [String],
+        requred: true,
+    },
+    affiliation:{
+        type: String,
+        required: false,
+    },
+    file:{
+        type: String,
+        required: true,
+    },
+    submittedBy:{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User",
+        required: true,
+    },
+    status:{
+        type: String,
+        requred: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Submission = mongoose.model("Submission", submissionSchema);
+
+export default Submission;
