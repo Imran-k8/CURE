f62445b75f47159c4a2c332cabab8e14e0293c3d
Imran Khan <khanimran092606@gmail.com>
2025-03-13 23:52:44 -0400

    some other changes

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -57,7 +57,40 @@ export const protectRouteAdmin = async (req, res, next) => {
 
     next();
   } catch (error) {
-    console.log("Error in protectRoute middleware: ", error.message);
+    console.log("Error in protectRouteAdmin middleware: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const protectRouteVerified = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized - No Token Provided" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+    }
+
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if(!user.verified){
+        return res.status(404).json({message: "User not verified"});
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log("Error in protectRouteVerified middleware: ", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
 };
---
+++ backend/routes/submission.routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import {protectRoute, protectRouteAdmin, protectRouteVerified} from "../middleware/auth.middleware.js"
+const router = express.Router();
+
+router.post("");
+
+
+export default router;
