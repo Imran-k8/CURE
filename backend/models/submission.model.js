34eb8b14d0f3b2723134d68f621316e63d4d9511
Imran Khan <khanimran092606@gmail.com>
2025-04-27 16:32:49 -0400

    indexing title and affiliation

--- backend/models/submission.model.js
+++ backend/models/submission.model.js
@@ -42,7 +42,7 @@ const submissionSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-submissionSchema.index({ authors: 'text', keywords: 'text', abstract: 'text' });
+submissionSchema.index({ authors: 'text', keywords: 'text', abstract: 'text', title: 'text', affiliation: 'text' });
 const Submission = mongoose.model("Submission", submissionSchema);
 
 export default Submission;
